88c2823e5043f1c688486b76e9c6e2d905d8c9c4
Christopher Lane <75538282+chrisallen12789@users.noreply.github.com>
2026-07-06 08:47:59 -0400

    Initialize canonical assurance

--- kernel/execution/canonical-assurance.js
+++ kernel/execution/canonical-assurance.js
@@ -1,1 +1,5 @@
+const { sha256 } = require("../lib/append-only-log");
+const { decodeWriteContent } = require("../transactions/validate");
+const { fileState } = require("./file-state");
+
 module.exports = {};
